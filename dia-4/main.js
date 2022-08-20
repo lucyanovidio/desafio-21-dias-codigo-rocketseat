89bbd2a75a19d57c1a900c2bc0f5ed9ebbe7cd83
Lucyan Ovídio <106949997+lucyanovidio@users.noreply.github.com>
2022-08-20 00:01:02 -0300

    Update main.js

--- dia-4/main.js
+++ dia-4/main.js
@@ -3,14 +3,12 @@ function insert(num) {
   document.querySelector('#resoult').innerHTML = numero + num
 }
 
-// não podia ser "clear" por algum metodo do html (?)
 function clean() {
   document.querySelector('#resoult').innerHTML = ''
 }
 
 function back() {
   let resoult = document.querySelector('#resoult').innerHTML
-  // ele vai pegar o último dos que tem de length e vai tirar com a função substring
   document.querySelector('#resoult').innerHTML = resoult.substring(
     0,
     resoult.length - 1
@@ -20,11 +18,8 @@ function back() {
 function calculate() {
   let resoult = document.querySelector('#resoult').innerHTML
   if (resoult) {
-    // o eva já faz todos os calculos pra gente
     document.querySelector('#resoult').innerHTML = eval(resoult)
   } else {
     document.querySelector('#resoult').innerHTML = ''
   }
 }
-
-// COISAS QUE FALTARAM: Qnd eu termino um calculo dps q aperto =, o próximo numero que eui apertar, eu quero q apague o que tiver lá
