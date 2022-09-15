8a3d46594d1f635cba3114ae70451d4d15eb941a
Lucyan Ovídio <106949997+lucyanovidio@users.noreply.github.com>
2022-09-15 20:11:09 -0300

    Update main.js

--- dia-17/main.js
+++ dia-17/main.js
@@ -1,6 +1,5 @@
 const key = document.querySelector('#tecla')
 
-// MANO COMO ASSSIIIIIMMMMM, qnd eu tinha colocado tecla ele n tava indo, aí eu coloquei "key" e ele foi, como se o próprio js já entendesse oq "key" significa, eu n entendo isso mano ????
 document.addEventListener('keydown', function (e) {
   key.innerHTML = e.key
   if (e.keyCode == 32) {
