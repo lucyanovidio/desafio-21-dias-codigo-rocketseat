3bd272c10c7dcb915f825723141ad45d2f0f716b
Lucyan Ovídio <106949997+lucyanovidio@users.noreply.github.com>
2022-09-24 22:09:47 -0300

    Update main.js

--- dia-20/main.js
+++ dia-20/main.js
@@ -1,9 +1,3 @@
-// O "EmojiButton" ta ficando subinhado e avisando no consolu q n foi declarado, e sai quando eu uso aquele import, mas eu n to sabendo usar o import (e eu achava q n ia precisar pq eu chamei o script do cdn no html)
-// o righ-start funcionou, mas outras coisas n funcionam ali n sei pq
-// a caixa de emojis tinha ficado pequena e algumas coisas tavam transbordando, daí tive que arrumar no css pegando o nome das classes conforme fui vendo pelo dev tools
-// n entendo ainda oq esse bglh de emoji button é, pq n é lib nem framework, sla oq é,e  n endendo como funciona isso de usar algo externo. Como é isso do cdn, e quais outros q funcionam tbm. 
-// tbm n sei fazer leitura de documentação
-
 const container = document.querySelector('.emoji-container')
 const copyBtn = document.querySelector('.copy-btn')
 const emojiBtn = document.querySelector('.emoji-btn')
@@ -22,7 +16,6 @@ emojiBtn.addEventListener('click', () => {
   } else {
     picker.showPicker(container)
   }
-  // picker.pickerVisible ? picker.hidePicker() : picker.showPicker(input)
 })
 
 copyBtn.addEventListener('click', async () => {
